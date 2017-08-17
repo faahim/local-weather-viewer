9c3e3b7ad5e4f0d0b5a6ce61e51fbc04c7887281
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-08-17 15:16:15 +0600

    We're now fetching users location along with weather data from two different source depending on GPS permisson.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
 $(document).ready(function() {
+	var lat;
+	var lon;
 
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function (position) {
-			var lati = position.coords.latitude;
-			var long = position.coords.longitude;
+	function locateYou() {
+
+		var ipApiCall = "https://ipapi.co/json";
+		$.getJSON(ipApiCall, function(ipData){
+			lat = ipData.latitude;
+			lon = ipData.longitude;
+			console.log(lat+" "+lon+"ip");
+			yourAddress();
+			getWearher();
+		});
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				lat = position.coords.latitude;
+				lon = position.coords.longitude;
+				console.log(lat+" "+lon+"geo");
+				yourAddress();
+				getWearher();
+			});
+		}
+	}
+
+	function yourAddress() {
+		var googleApiCall = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon;
+		$.getJSON(googleApiCall, function(locationName){
+			$(".locName").html(locationName.results[2].formatted_address);
+			console.log(locationName.results[2].formatted_address);
 		});
 	}
+
+	function getWearher() {
+		var weatherApiKey = "a3219d4e2772db6e34c6491e62144b27";
+		var weatherApiCall = "https://api.darksky.net/forecast/"+weatherApiKey+"/"+lat+","+lon;
+		$.ajax({
+			url: weatherApiCall,
+			type: "GET",
+			dataType: "jsonp",
+			success: function(weatherData) {
+				$(".currentTemp").html(weatherData.currently.temperature);
+				$(".feelsLike").html(weatherData.currently.apparentTemperature);
+				$(".humidity").html(weatherData.currently.humidity);
+				$(".windSpeed").html(weatherData.currently.windSpeed);
+			}
+		});
+	}
+
+	locateYou();
 
 });
